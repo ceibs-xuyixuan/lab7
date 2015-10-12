2ce6e93f8799b9f83ac3598534b59ae2cf088302
xuyixuan <yixxu916@student.liu.se>
2015-10-12 21:55:20 +0200

    dplyr

---
+++ ridge/R/delays.R
@@ -0,0 +1,15 @@
+library("dplyr")
+visualize_airpart_delays <- function(){
+  data("airports")
+  data("flights")
+  airports_filter <- filter(airports, dst == "A")
+  flights_filter <- filter(flights, month >= 3)
+  flights_filter <- filter(flights, month <= 11)
+  flights_filter <- filter(flights, month == 3 & day >= 11)
+  flights_filter <- filter(flights, month == 11 & day <= 3)
+  airports_select <- select(airports_filter, one_of(c("faa","lat","lon")))
+  flights_select <- select(flights_filter, one_of(c("dest", "dep_delay", "arr_delay")))
+  all_dest <- inner_join(flights_select, airports_select, by =c("dest" = "faa"))
+  a <- summarise(group_by(all_origin, dest), mean(dep_delay, na.rm = TRUE), lat=mean(lat), lon=mean(lon))
+  View(a)
+  }
